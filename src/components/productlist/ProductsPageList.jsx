85754c40c7bca09ff07793708e75246073d081f9
sejin <neokin7734@naver.com>
2025-03-22 13:55:07 +0900

    250322 박세진 페이지 상품 출력순서 조정

--- src/components/productlist/ProductsPageList.jsx
+++ src/components/productlist/ProductsPageList.jsx
@@ -11,6 +11,9 @@ const ProductsPageList = ({ loading, error }) => {
   const initPageNumber = Number(searchParams.get('page')) || 1;
   const { list } = useProductListStore();
 
+  // 페이지별 표시 항목수선택
+  const [pagePerItem, setPagePerItem] = useState(10);
+
   // 인원 수, 지역, 숙박일정은 쿼리로 변경
   // 캐시된 데이터에서 필터링을 숙소 타입과 금액범위으로 제한
   const { range, rangeLimit } = usePriceStore();
@@ -26,6 +29,7 @@ const ProductsPageList = ({ loading, error }) => {
       setFilterLoading(true);
       await accomCheck(array);
       setFiltered([...array]);
+      sortProduct(array);
       setFilterLoading(false);
     }
     filterWaiting();
@@ -41,6 +45,52 @@ const ProductsPageList = ({ loading, error }) => {
   const getPrice = useCallback((val) => {
     return val * 10000;
   });
+
+  // 출력전 상품 기본 정렬.
+  const sortProduct = (array, category = 'price', orderBy = 'asc') => {
+    switch (category) {
+      case 'price':
+        // 기본설정 오름차순
+        switch (orderBy) {
+          case 'desc':
+            return array.sort((a, b) => b.final_price - a.final_price);
+          default:
+            return array.sort((a, b) => a.final_price - b.final_price);
+        }
+      // 기본설정 내림차순
+      case 'discount':
+        switch (orderBy) {
+          case 'desc':
+            return array.sort((a, b) => b.discount_rate - a.discount_rate);
+          default:
+            return array.sort((a, b) => a.discount_rate - b.discount_rate);
+        }
+      // 기본설정 내림차순
+      case 'rating':
+        switch (orderBy) {
+          case 'asc':
+            return array.sort((a, b) => a.rating - b.rating);
+          default:
+            return array.sort((a, b) => b.rating - a.rating);
+        }
+      case 'reviewsCount':
+        switch (orderBy) {
+          // 기본설정 내림차순
+          default:
+            return array.sort((a, b) => b.reviews_count - a.reviews_count);
+          case 'asc':
+            return array.sort((a, b) => a.reviews_count - b.reviews_count);
+        }
+      // 기본설정 가격, 오름차순
+      default:
+        switch (orderBy) {
+          case 'desc':
+            return array.sort((a, b) => b.final_price - a.final_price);
+          default:
+            return array.sort((a, b) => a.final_price - b.final_price);
+        }
+    }
+  };
 
   const accomCheck = (array) => {
     const start = new Date().getMilliseconds();
@@ -94,26 +144,26 @@ const ProductsPageList = ({ loading, error }) => {
 
   return (
     <>
-      {/* 할인률 , 평점 , 리뷰수 정렬 UI 추가 예정 */}
-      {!filterLoading &&
-        filtered.map((ele) => {
-          <p> {ele} </p>;
-        })}
+      {/* 할인률 , 평점 , 리뷰수 정렬, 목록 5,10,15 개씩 보기 UI 추가 예정 */}
+      {filtered.length > 0 &&
+        console.log(
+          '정렬순서(오름차순, 내림차순), 정렬항목(가격, 평점, 할인률, 리뷰수) , 보이는 목록갯수 설정',
+        )}
       <ul>
         {filtered.length > 0 &&
           filtered
             .filter(
               (_, index) =>
-                (initPageNumber - 1) * 10 <= index &&
-                index < (initPageNumber - 1) * 10 + 10,
+                (initPageNumber - 1) * pagePerItem <= index &&
+                index < (initPageNumber - 1) * pagePerItem + pagePerItem,
             )
             .map((product, index, array) => (
               <ProductCard
-                key={index}
+                key={product.id}
                 index={index}
                 product={product}
                 arrayLength={array.length}
-                ref={focus}
+                // ref={focus}
               />
             ))}
       </ul>
@@ -121,6 +171,7 @@ const ProductsPageList = ({ loading, error }) => {
         <Pagination
           data={filtered}
           focus={focus}
+          pagePerItem={pagePerItem}
         />
       )}
     </>

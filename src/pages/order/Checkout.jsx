3abb4dc6371b55b3e1a65ca8e57b4d5c90bd379d
sejin <neokin7734@naver.com>
2025-03-25 10:17:46 +0900

    250325 박세진 결제페이지 결제포인트 컴포넌트 분리

--- src/pages/order/Checkout.jsx
+++ src/pages/order/Checkout.jsx
@@ -7,7 +7,7 @@ import KakaoMap from '../../components/common/KakaoMap';
 import PeopleSelector from '../../components/common/PeopleSelector';
 import ServiceList from '../../components/common/ServiceList';
 import OrderList from '../../components/order/checkout/OrderList';
-import OrderSummary from '../../components/order/checkout/OrderSummary';
+import OrderSummaryChart from '../../components/order/checkout/OrderSummaryChart';
 import { useAccomData } from '../../hooks/useProductData';
 import useReservationStore from '../../stores/useReservationStore';
 
@@ -237,31 +237,7 @@ const Checkout = () => {
 
         {/* 최종 결제 금액 */}
         <div className="sticky card top-15 bg-base-100 w-96 shadow-sm dark:border-gray-400 dark:border-1">
-          {reservationData.length > 0 &&
-            reservationData.map((ele) => {
-              const { data, isLoading, error } = useAccomData(
-                ele.accommodationId,
-              );
-
-              // 상품페이지 -> 결제정보 -> 뒤로가기 -> 앞으로가기
-              // 히스토리만 추적하다, 전달정보가 없는경우
-              if (data === undefined) return <></>;
-
-              return (
-                <form className="card-body">
-                  <OrderSummary orderInfo={[reservationData]} />
-
-                  <div className="card-actions justify-end">
-                    <button
-                      type="button"
-                      onClick={() => navigate('/orderconfirmation')}
-                      className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden">
-                      결제하기
-                    </button>
-                  </div>
-                </form>
-              );
-            })}
+          <OrderSummaryChart reservationData={reservationData} />
         </div>
       </div>
     </div>

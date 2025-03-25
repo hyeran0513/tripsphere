8a8cdb00858c21e5abe51cc2ed06c5ddbcdff4a8
sejin <neokin7734@naver.com>
2025-03-25 17:26:47 +0900

    250325 박세진 결제페이지 사용자 포인트 조회, 차감, 내역 생성 작업 진행중

--- src/components/order/checkout/OrderSummary.jsx
+++ src/components/order/checkout/OrderSummary.jsx
@@ -1,12 +1,31 @@
-const OrderSummary = (
-  //[orderInfo]
-  orderInfo,
-) => {
-  let price = 0;
-  console.log(orderInfo);
-  // const sum = orderInfo.map((ele) => price + parseInt(ele.final_price));
-  const sum = orderInfo.final_price;
-  const commision = Math.ceil(sum / 10);
+import { useEffect, useState } from 'react';
+import { fetchUserData } from '../../../services/userService';
+import useAuthStore from '../../../stores/useAuthStore';
+
+const OrderSummary = ({ orderInfo }) => {
+  const [price, setPrice] = useState(0);
+  const [commision, setCommision] = useState(0);
+  const [userInfo, setUserInfo] = useState(null);
+  const { user } = useAuthStore();
+
+  const { email, uid, token } = user;
+
+  useEffect(() => {
+    let sum = 0;
+    orderInfo.map((ele) => {
+      sum += parseInt(ele.totalPrice);
+    });
+    setPrice(sum);
+    setCommision(Math.ceil(sum / 10));
+
+    console.log('userInfo : ', JSON.stringify(userInfo));
+    console.log(JSON.stringify(setUserInfo(fetchUserData(uid))));
+    console.log(userDataWait(uid));
+  }, []);
+
+  const userDataWait = async (a) => {
+    return await fetchUserData(uid);
+  };
 
   return (
     <div className="dark:font-bold">
@@ -14,8 +33,7 @@ const OrderSummary = (
 
       <div className="flex justify-between py-2">
         <p>주문 금액</p>
-        {/* <p className="flex justify-end">100000 원</p> */}
-        <p className="flex justify-end">{sum} 원</p>
+        <p className="flex justify-end">{price}원</p>
       </div>
 
       <div className="flex justify-between py-2">
@@ -26,19 +44,19 @@ const OrderSummary = (
       <div className="border-t border-gray-200">
         <div className="flex justify-between py-4">
           <p>주문 총액</p>
-          <p className="flex justify-end">{sum + commision}원</p>
+          <p className="flex justify-end">{price + commision}원</p>
         </div>
 
         <div className="flex justify-between py-4 text-red-600 dark:text-red-400">
           <p>사용 포인트</p>
-          <p className="flex justify-end">{sum + commision}원</p>
+          <p className="flex justify-end">{price + commision}원</p>
         </div>
       </div>
 
       <div className="border-t border-gray-200">
         <div className="flex justify-between py-4">
-          <p>잔여 포인트</p>
-          <p className="flex justify-end">{sum + commision}원</p>
+          <p>결제 후 잔여 포인트</p>
+          <p className="flex justify-end">{price + commision}원</p>
         </div>
       </div>
     </div>

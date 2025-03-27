707566fc12fb89398c4bb77251358e01e39ecbf2
sejin <neokin7734@naver.com>
2025-03-27 15:57:27 +0900

    250327 박세진 숙소 및 객실정보 조회 반영
    250327 박세진 테스트 숙소정보 쿼리 조회결과 주석 첨부

--- src/services/productListService.jsx
+++ src/services/productListService.jsx
@@ -18,8 +18,8 @@ export const getAllAccomData = async ({
   const accomDoc = collection(db, 'accommodations');
   let constraints = [];
 
-  constraints.push(where('capacity.adults', '>=', adultCount));
-  constraints.push(where('capacity.children', '>=', childrenCount));
+  // constraints.push(where('capacity.adults', '>=', adultCount));
+  // constraints.push(where('capacity.children', '>=', childrenCount));
 
   // 도시 대분류 필터
   if (selectedCity === '전체' || selectedCity === '') {
@@ -37,23 +37,23 @@ export const getAllAccomData = async ({
   const today = new Date();
   if (checkIn === '') {
     console.log('DATE is null. initialize : ', today);
-    constraints.push(where('check_in', '>=', Timestamp.fromDate(today)));
+    // constraints.push(where('check_in', '>=', Timestamp.fromDate(today)));
   } else {
     // 기본값 설정
-    constraints.push(
-      where('check_in', '>=', Timestamp.fromDate(new Date(checkIn))),
-    );
+    // constraints.push(
+    //   where('check_in', '>=', Timestamp.fromDate(new Date(checkIn))),
+    // );
   }
 
   // 체크아웃 기본값 설정
-  let checkOutTimestamp;
-  // Firestore에서 필터링하지 않고, 이후 클라이언트에서 필터링
-  if (checkOut === '') {
-    today.setDate(today.getDate() + 1);
-    checkOutTimestamp = Timestamp.fromDate(today);
-  } else {
-    checkOutTimestamp = Timestamp.fromDate(new Date(checkOut));
-  }
+  // let checkOutTimestamp;
+  // // Firestore에서 필터링하지 않고, 이후 클라이언트에서 필터링
+  // if (checkOut === '') {
+  //   today.setDate(today.getDate() + 1);
+  //   checkOutTimestamp = Timestamp.fromDate(today);
+  // } else {
+  //   checkOutTimestamp = Timestamp.fromDate(new Date(checkOut));
+  // }
 
   // console.log('쿼리 조건 : ', ...constraints);
 
@@ -66,32 +66,59 @@ export const getAllAccomData = async ({
   try {
     // 숙소정보 검색
     const accomSnap = await getDocs(q);
-    // console.log('쿼리 결과', accomSnap);
-    results = accomSnap.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-
-    // 숙소정보 숙박 가능일자로 필터링
-    // if (checkOutTimestamp) {
-    results = results.filter((accom) => {
-      accom !==
-        (Timestamp.fromDate(new Date(checkIn)) <=
-          accom.booked_dates[0].toMillis() &&
-          accom.booked_dates[1].toMillis() <= checkOutTimestamp);
-
-      accomIds.push(accom.id);
+    console.log('쿼리 결과', accomSnap);
+    results = accomSnap.docs.map((doc) => {
+      accomIds.push(doc.id);
+      return { id: doc.id, ...doc.data() };
     });
-    // }
 
-    // return results;
+    console.log(' 숙소정보 조회 결과 : ', results);
+    console.log(' 숙소 조회 결과 아이디들 : ', accomIds);
   } catch (error) {
     console.error('숙소 정보 조회 오류', error);
     return [];
   }
-  if (results === null) {
+
+  if (results === null || accomIds.length === 0) {
+    console.log('조회된 숙소 정보가 없음');
     return [];
   }
 
-  let rooms = null;
+  let rooms = [];
   try {
+    let roomConstraints = [];
+
+    // 숙소 아이디 기반 필터링
+    if (accomIds.length > 0) {
+      roomConstraints.push(where('accommodation_id', 'in', accomIds));
+    }
+
+    // 체크인 날짜와 체크아웃 날짜 필터링
+    const today = new Date();
+    if (checkIn === '') {
+      console.log('DATE is null. room checkin initialize : ', today);
+      constraints.push(where('check_in', '>=', Timestamp.fromDate(today)));
+    } else {
+      // 기본값 설정
+      constraints.push(
+        where('check_in', '>=', Timestamp.fromDate(new Date(checkIn))),
+      );
+    }
+
+    // 체크아웃 기본값 설정
+    let checkOutTimestamp;
+    // Firestore에서 필터링하지 않고, 이후 클라이언트에서 필터링
+    if (checkOut === '') {
+      today.setDate(today.getDate() + 1);
+      checkOutTimestamp = Timestamp.fromDate(today);
+    } else {
+      checkOutTimestamp = Timestamp.fromDate(new Date(checkOut));
+    }
+
+    // 어른 수
+    roomConstraints.push(where('capacity.adults', '>=', adultCount));
+    roomConstraints.push(where('capacity.children', '>=', childrenCount));
+
     // 객실 쿼리
     const roomQuery = query(collection(db, 'rooms'), ...roomConstraints);
     const roomSnap = await getDocs(roomQuery);
@@ -100,7 +127,7 @@ export const getAllAccomData = async ({
     console.log('rooms before filtering: ', rooms);
 
     // 숙소마다 연결된 객실 정보 붙이기
-    const results = accommodations.map((accom) => {
+    const accomList = results.map((accom) => {
       // 각 숙소에 맞는 객실들을 필터링하여 매칭
       const accomRooms = rooms.filter(
         (room) => room.accommodation_id === accom.id,
@@ -112,12 +139,83 @@ export const getAllAccomData = async ({
       };
     });
 
-    console.log('getAccomData 최종: ' + JSON.stringify(results));
+    console.log('getAllAccomData 최종: ' + JSON.stringify(accomList));
   } catch (error) {
     console.error('객실 정보 조회 오류', error);
     return [];
   }
 };
+
+/*
+ 테스트 데이터 쿼리 결과 확인
+  {
+    "id": "testhotel1",
+    "description": "example",
+    "type": "hotel",
+    "name": "testhotel1",
+    "rating ": 4.8,
+    "location": {
+      "latitude": 31.111111,
+      "longitude": "123.111111",
+      "sub_city": "강남구",
+      "city": "서울",
+      "place_name": "XX동"
+    },
+    "host": {
+      "name": "example",
+      "contact": "010-0000-0000",
+      "email": "example@test.com"
+    },
+    "stay_type": "stay",
+    "images": [
+      "img.example.com"
+    ],
+    "rooms": [
+      {
+        "id": "MkBqyIvCtjTY0ENGZLTm",
+        "original_price": 100000,
+        "capacity": {
+          "adults": 2,
+          "children": 2
+        },
+        "services": [
+          "wifi",
+          "tv",
+          "mini_bar"
+        ],
+        "room_id": "testhotel1-1",
+        "type": "suite",
+        "images": "img.example.com",
+        "name": "스위트 룸",
+        "booked_dates ": [
+          {
+            "check_out": {
+              "seconds": 1743400133,
+              "nanoseconds": 320000000
+            },
+            "check_in": {
+              "seconds": 1743274183,
+              "nanoseconds": 595000000
+            }
+          }
+        ],
+        "description": "example",
+        "discount_rate": 0.1,
+        "check_out": {
+          "seconds": 1743487200,
+          "nanoseconds": 96000000
+        },
+        "accommodation_id": "testhotel1",
+        "stock": "3",
+        "check_in": {
+          "seconds": 1743141600,
+          "nanoseconds": 302000000
+        },
+        "availability": "true"
+      }
+    ]
+  },
+*/
 
 export const getAllProductData = async ({
   selectedCity = '전체',

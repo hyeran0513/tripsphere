f2e9810ff54945057aabecd87d0370424ebdbe09
hyeran0513 <hr_0513@naver.com>
2025-03-29 14:14:30 +0900

    250329 김혜란 로그인하지 않은 상태로 찜 버튼 클릭 시, 경고 토스트 메시지 출력 및 동작 제한

--- src/components/detail/header/ProductHeader.jsx
+++ src/components/detail/header/ProductHeader.jsx
@@ -27,6 +27,11 @@ const ProductHeader = ({ product, productId, hideFavorite }) => {
   const handleFavorite = (e) => {
     if (e) e.preventDefault();
 
+    if (!isAuthenticated) {
+      showToast('error', '로그인 후 찜 기능을 이용하실 수 있습니다.');
+      return;
+    }
+
     favoriteMutation(productId);
   };
 
@@ -41,45 +46,43 @@ const ProductHeader = ({ product, productId, hideFavorite }) => {
         </div>
 
         {/* 로그인할 경우에만, 버튼 노출 */}
-        {isAuthenticated && (
-          <div className="mt-5 flex lg:mt-0 lg:ml-4">
-            {/* 카카오 공유하기 버튼 */}
-            <span className="hidden sm:block">
-              {product?.images.length > 0 && (
-                <KakaoShareButton
-                  hasText
-                  title={product.name}
-                  description={product.description}
-                  imageUrl={product?.images[0]}
-                  pageUrl={window.location.origin + '/product/' + productId}
-                />
-              )}
-            </span>
-
-            {/* 찜하기 버튼 */}
-            {!hideFavorite && (
-              <span className="ml-3 hidden sm:block">
-                <button
-                  onClick={handleFavorite}
-                  type="button"
-                  className="cursor-pointer dark:text-white inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50">
-                  {isFavorite ? (
-                    <BiSolidHeart
-                      aria-hidden="true"
-                      className="mr-1.5 -ml-0.5 size-5 text-red-400"
-                    />
-                  ) : (
-                    <BiHeart
-                      aria-hidden="false"
-                      className="mr-1.5 -ml-0.5 size-5 text-gray-400"
-                    />
-                  )}
-                  찜하기
-                </button>
-              </span>
+        <div className="mt-5 flex lg:mt-0 lg:ml-4">
+          {/* 카카오 공유하기 버튼 */}
+          <span className="hidden sm:block">
+            {product?.images.length > 0 && (
+              <KakaoShareButton
+                hasText
+                title={product.name}
+                description={product.description}
+                imageUrl={product?.images[0]}
+                pageUrl={window.location.origin + '/product/' + productId}
+              />
             )}
-          </div>
-        )}
+          </span>
+
+          {/* 찜하기 버튼 */}
+          {!hideFavorite && (
+            <span className="ml-3 hidden sm:block">
+              <button
+                onClick={handleFavorite}
+                type="button"
+                className="cursor-pointer dark:text-white inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50">
+                {isFavorite ? (
+                  <BiSolidHeart
+                    aria-hidden="true"
+                    className="mr-1.5 -ml-0.5 size-5 text-red-400"
+                  />
+                ) : (
+                  <BiHeart
+                    aria-hidden="false"
+                    className="mr-1.5 -ml-0.5 size-5 text-gray-400"
+                  />
+                )}
+                찜하기
+              </button>
+            </span>
+          )}
+        </div>
       </div>
 
       {/* 토스트 메시지 */}

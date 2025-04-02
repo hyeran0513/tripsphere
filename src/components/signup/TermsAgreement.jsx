237402d10af55b36b22f6c19a0c286eabcb00fce
hyeran0513 <hr_0513@naver.com>
2025-04-02 17:56:04 +0900

    250402 김혜란 이용약관 내용 추가 및 적용

--- src/components/signup/TermsAgreement.jsx
+++ src/components/signup/TermsAgreement.jsx
@@ -1,42 +1,34 @@
 import { useState } from 'react';
 import { BiChevronRight } from 'react-icons/bi';
-
-// 약관 목록
-const list = [
-  '서비스 이용약관',
-  '개인정보 수집/이용 동의',
-  '개인정보 제3자 정보 제공 동의',
-  '위치 기반 서비스 이용 약관 동의',
-];
+import Modal from '../common/Modal';
+import { terms } from '../data/terms';
 
 const TermsAgreement = ({ onAgree }) => {
   const [isChecked, setIsChecked] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const [checkedItems, setCheckedItems] = useState(() =>
-    Object.fromEntries(list.map((item) => [item, false])),
+    Object.fromEntries(terms.map((item) => [item.title, false])),
   );
   const [isAllChecked, setIsAllChecked] = useState(false);
 
   // 동의 버튼 클릭
   const handleAgree = () => {
     setIsChecked(true);
-    setIsModalOpen(false);
+    document.getElementById('termsAgreementModal').close();
     onAgree?.();
   };
-
-  // 모달 닫기
-  const handleCloseModal = () => setIsModalOpen(false);
 
   // 모든 항목 동의 체크박스 변경
   const handleAllAgreeChange = () => {
     const newState = !isAllChecked;
     setIsAllChecked(newState);
-    setCheckedItems(Object.fromEntries(list.map((item) => [item, newState])));
+    setCheckedItems(
+      Object.fromEntries(terms.map((item) => [item.title, newState])),
+    );
   };
 
   // 개별 항목 체크박스 변경
-  const handleItemChange = (item) => {
-    const updatedItems = { ...checkedItems, [item]: !checkedItems[item] };
+  const handleItemChange = (title) => {
+    const updatedItems = { ...checkedItems, [title]: !checkedItems[title] };
     setCheckedItems(updatedItems);
     setIsAllChecked(Object.values(updatedItems).every(Boolean));
   };
@@ -49,9 +41,10 @@ const TermsAgreement = ({ onAgree }) => {
           <input
             id="link-checkbox"
             type="checkbox"
-            value=""
             checked={isChecked}
-            onChange={() => document.getElementById('my_modal_1').showModal()}
+            onClick={() =>
+              document.getElementById('termsAgreementModal').showModal()
+            }
             className="checkbox checkbox-primary"
           />
           <label
@@ -63,7 +56,7 @@ const TermsAgreement = ({ onAgree }) => {
 
         {/* 약관 동의 모달 */}
         <dialog
-          id="my_modal_1"
+          id="termsAgreementModal"
           className="modal">
           <div className="modal-box">
             <h3 className="font-bold text-lg">약관 동의</h3>
@@ -81,7 +74,6 @@ const TermsAgreement = ({ onAgree }) => {
                   onChange={handleAllAgreeChange}
                   className="checkbox checkbox-primary"
                 />
-
                 <label
                   htmlFor="all-agree"
                   className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
@@ -92,48 +84,76 @@ const TermsAgreement = ({ onAgree }) => {
               {/* 약관 목록 */}
               <div>
                 <ul className="list">
-                  {list.map((item) => (
+                  {terms.map((item, index) => (
                     <li
-                      key={item}
+                      key={item.title}
                       className="list-row flex items-center justify-between">
                       <div className="flex items-center">
                         <input
                           type="checkbox"
-                          checked={checkedItems[item]}
-                          onChange={() => handleItemChange(item)}
+                          checked={checkedItems[item.title]}
+                          onChange={() => handleItemChange(item.title)}
                           className="checkbox checkbox-primary"
                         />
 
                         <label className="flex items-center gap-2 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                           <div className="badge badge-soft badge-info">
                             필수
-                          </div>{' '}
-                          {item}
+                          </div>
+                          {item.title}
                         </label>
                       </div>
 
-                      <button className="btn btn-square btn-ghost">
+                      {/* 이용약관 상세 보기 버튼 */}
+                      <button
+                        type="button"
+                        className="btn btn-square btn-ghost"
+                        onClick={() =>
+                          document.getElementById(`term${index}`).showModal()
+                        }>
                         <BiChevronRight className="size-8" />
                       </button>
+
+                      <dialog
+                        id={`term${index}`}
+                        className="modal">
+                        <div className="modal-box">
+                          {/* 이용약관 제목 */}
+                          <h3 className="font-bold text-lg">{item?.title}</h3>
+
+                          {/* 이용약관 설명 */}
+                          <pre className="py-4 whitespace-pre-wrap break-words">
+                            {item?.description}
+                          </pre>
+
+                          {/* 닫기 버튼 */}
+                          <div className="modal-action">
+                            <form method="dialog">
+                              <button className="btn">닫기</button>
+                            </form>
+                          </div>
+                        </div>
+                      </dialog>
                     </li>
                   ))}
                 </ul>
               </div>
             </div>
 
+            {/* 모달 버튼 */}
             <div className="modal-action">
               <form method="dialog">
                 <div className="flex items-center gap-2">
                   <button
                     className="btn"
-                    onClick={handleCloseModal}>
+                    onClick={() =>
+                      document.getElementById('termsAgreementModal').close()
+                    }>
                     취소
                   </button>
 
                   <button
-                    className={`btn btn-primary ${
-                      !isAllChecked ? 'btn-disabled' : ''
-                    }`}
+                    className={`btn btn-primary ${!isAllChecked ? 'btn-disabled' : ''}`}
                     onClick={handleAgree}
                     disabled={!isAllChecked}>
                     동의

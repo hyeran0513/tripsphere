7967bd71f79e8367cda4826a3852c5cad95c0e2c
hyeran0513 <hr_0513@naver.com>
2025-04-03 10:37:21 +0900

    250403 김혜란 useFilterStore.js, useReservationStore.js 타입 잘못 들어왔을 때에 대비로 타입 형변환

--- src/stores/useFilterStore.js
+++ src/stores/useFilterStore.js
@@ -9,22 +9,33 @@ const useFilterStore = create((set) => ({
   checkOut: new Date().toISOString().split('T')[0],
 
   // 지역 대분류(시·도) 변경
-  setSelectedCity: (city) => set({ selectedCity: city }),
+  setSelectedCity: (city) => set({ selectedCity: String(city).trim() }),
 
   // 지역 소분류(시·군·구) 변경
-  setSelectedSubCity: (subCity) => set({ selectedSubCity: subCity }),
+  setSelectedSubCity: (subCity) =>
+    set({ selectedSubCity: String(subCity).trim() }),
 
   // 어른 수 변경
-  setAdultCount: (num) => set({ adultCount: num }),
+  setAdultCount: (num) => set({ adultCount: Number(num) || 0 }),
 
   // 미성년자 수 변경
-  setChildrenCount: (num) => set({ childrenCount: num }),
+  setChildrenCount: (num) => set({ childrenCount: Number(num) || 0 }),
 
   // 체크인 날짜 변경
-  setCheckIn: (date) => set({ checkIn: date }),
+  setCheckIn: (date) =>
+    set({
+      checkIn:
+        new Date(date).toISOString().split('T')[0] ||
+        new Date().toISOString().split('T')[0],
+    }),
 
   // 체크아웃 날짜 변경
-  setCheckOut: (date) => set({ checkOut: date }),
+  setCheckOut: (date) =>
+    set({
+      checkOut:
+        new Date(date).toISOString().split('T')[0] ||
+        new Date().toISOString().split('T')[0],
+    }),
 
   // 상태 초기화
   resetFilter: () =>
--- src/stores/useReservationStore.js
+++ src/stores/useReservationStore.js
@@ -9,22 +9,42 @@ const useReservationStore = create((set) => ({
   accommodationId: '',
 
   // 어른 수 변경
-  setAdultCount: (num) => set({ adultCount: num }),
+  setAdultCount: (num) => {
+    set({ adultCount: Number(num) || 0 });
+  },
 
   // 미성년자 수 변경
-  setChildrenCount: (num) => set({ childrenCount: num }),
+  setChildrenCount: (num) => {
+    set({ childrenCount: Number(num) || 0 });
+  },
 
   // 체크인 날짜 변경
-  setCheckIn: (date) => set({ checkIn: date }),
+  setCheckIn: (date) => {
+    set({
+      checkIn:
+        new Date(date).toISOString().split('T')[0] ||
+        new Date().toISOString().split('T')[0],
+    });
+  },
 
   // 체크아웃 날짜 변경
-  setCheckOut: (date) => set({ checkOut: date }),
+  setCheckOut: (date) => {
+    set({
+      checkOut:
+        new Date(date).toISOString().split('T')[0] ||
+        new Date().toISOString().split('T')[0],
+    });
+  },
 
   // 총 예약 금액
-  setTotalPrice: (num) => set({ totalPrice: num }),
+  setTotalPrice: (num) => {
+    set({ totalPrice: Number(num) || 0 });
+  },
 
   // 상품 ID
-  setAccommodationId: (num) => set({ accommodationId: num }),
+  setAccommodationId: (id) => {
+    set({ accommodationId: String(id).trim() || '' });
+  },
 
   // 상태 초기화
   resetReservation: () =>
